d6d81bac38b73f765a0a960bfa7453e8629b535a
“Mark <mkanko66@gmail.com>
2020-06-19 12:42:40 -0700

    lab completed

--- main.js
+++ main.js
@@ -4,8 +4,34 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener('DOMContentLoaded', () => {
+   setEventListeners()
+})
 
+const setEventListeners = () => {
+  const glyphs = document.querySelectorAll('.like-glyph')
+  for (let glyph of glyphs) {
+    glyph.addEventListener('click', postLike)
+  } 
+}
 
+const postLike = event => {
+  const glyph = event.target 
+  mimicServerCall()
+  .then(resp => {
+    glyph.innerHTML = glyph.innerHTML === EMPTY_HEART ?  FULL_HEART : EMPTY_HEART 
+    glyph.classList.toggle('activated-heart')
+  })
+
+  .catch(error => {
+    const modalDiv = document.getElementById('modal')
+    const modalP = document.getElementById('modal-message')
+    modalDiv.classList.remove('hidden')
+    modalP.innerText = error 
+    setTimeout(() => modalDiv.classList.add('hidden'), 5000)
+    
+  })
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
